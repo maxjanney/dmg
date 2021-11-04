60c127d8f082acb046843688042cbef3c02415c8
maxjanney <majanney@me.com>
2021-11-04 13:06:53 -0400

    writing for each mbc

--- src/mem.rs
+++ src/mem.rs
@@ -1,3 +1,4 @@
+#[derive(PartialEq, Eq)]
 enum Mbc {
     Mbc1,
     Mbc2,
@@ -5,11 +6,28 @@ enum Mbc {
     Mbc5,
 }
 
+enum BankMode {
+    Rom,
+    Ram,
+}
+
+impl BankMode {
+    fn from_u8(val: u8) -> Self {
+        if val & 1 != 0 {
+            Self::Ram
+        } else {
+            Self::Rom
+        }
+    }
+}
+
 pub struct Memory {
     rom: Vec<u8>,
     ram: Vec<u8>,
     rom_bank: u16,
+    ram_bank: u8,
     ram_enabled: bool,
+    mode: BankMode,
     mbc: Mbc,
 }
 
@@ -53,23 +71,86 @@ impl Memory {
     // Write a byte at the specified address
     pub fn wb(&mut self, addr: u16, val: u8) {
         match addr {
+            // 0000-1FFF - RAM and Timer Enable (Write Only)
             0x0000..=0x1fff => match self.mbc {
-                Mbc::Mbc1 | Mbc::Mbc3 | Mbc::Mbc5 => self.ram_enabled = val & 0x0f == 0x0a,
+                Mbc::Mbc1 | Mbc::Mbc3 | Mbc::Mbc5 => self.ram_enabled = val & 0xf == 0xa,
                 Mbc::Mbc2 => {
                     if addr & 0x100 == 0 {
                         self.ram_enabled = !self.ram_enabled;
                     }
                 }
             },
-            0x2000..=0x3fff => match self.mbc {
+            // 2000-3FFF - ROM Bank Number (Write Only)
+            0x2000..=0x3fff => {
                 let val = val as u16;
-                Mbc::Mbc1 => {
-                    self.rom_bank = (self.rom_bank & 0x60) | (val & 0x1f);
-                    if self.rom_bank == 0 {
-                        self.rom_bank = 1;
+                match self.mbc {
+                    Mbc::Mbc1 => {
+                        self.rom_bank = (self.rom_bank & 0x60) | (val & 0x1f);
+                        if self.rom_bank == 0 {
+                            self.rom_bank = 1;
+                        }
+                    }
+                    Mbc::Mbc2 => {
+                        if addr & 0x100 != 0 {
+                            self.rom_bank = val & 0xf;
+                        }
+                    }
+                    Mbc::Mbc3 => {
+                        let val = if val == 0 { 1 } else { val };
+                        self.rom_bank = val & 0x7f;
+                    }
+                    Mbc::Mbc5 => {
+                        if addr >> 12 == 0x2 {
+                            // 2000 - 2FFF (ROM Bank Low)
+                            self.rom_bank = (self.rom_bank & 0xff00) | val;
+                        } else {
+                            // $3000 - $3FFF (ROM Bank High)
+                            let val = val & 1;
+                            self.rom_bank = (self.rom_bank & 0x00ff) | (val << 8);
+                        }
                     }
                 }
+            }
+            // 4000-5FFF - RAM Bank Number - or - Upper Bits of ROM Bank Number
+            0x4000..=0x5fff => match self.mbc {
+                Mbc::Mbc1 => match self.mode {
+                    BankMode::Rom => {
+                        let val = (val & 0x3) as u16;
+                        self.rom_bank = (self.rom_bank & 0x1f) | (val << 5);
+                    }
+                    BankMode::Ram => {
+                        self.ram_bank = val & 0x3;
+                    }
+                },
+                Mbc::Mbc3 => {
+                    self.ram_bank = val & 0x3;
+                    todo!("Rtc mapping");
+                }
+                Mbc::Mbc5 => {
+                    self.ram_bank = val & 0xf;
+                }
+                _ => {}
             },
+            // 6000-7FFF - ROM/RAM Mode Select (Write Only)
+            0x6000..=0x7fff => match self.mbc {
+                Mbc::Mbc1 => self.mode = BankMode::from_u8(val),
+                Mbc::Mbc3 => todo!("Rtc latch"),
+                _ => {}
+            },
+            // A000-BFFF - RAM Bank 00-03, if any (Read/Write)
+            0xa000..=0xbfff => {
+                if self.ram_enabled {
+                    // todo!("check if rtc is currently mapped to this region for MBC2");
+                    let val = if self.mbc == Mbc::Mbc2 {
+                        val & 0xf
+                    } else {
+                        val
+                    };
+                    let offset = (self.ram_bank as u16) * 0x2000;
+                    self.ram[((addr - 0xa000) + offset) as usize] = val;
+                }
+            }
+            _ => {}
         }
     }
 }
